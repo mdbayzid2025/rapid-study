70e01ab70377267a44cb247ef6307cc1eaf419f9
ssmdbayzid2025 <ssmd.bayzid23@gmail.com>
2025-07-14 18:35:57 +0600

    update

--- src/app/components/shared/Navbar/Navbar.tsx
+++ src/app/components/shared/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 import { IoCall, IoLogoYoutube } from 'react-icons/io5'
 import { MdOutlineMailOutline } from 'react-icons/md'
 import Container from '../Container/Container'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
@@ -19,7 +20,7 @@ const Navbar = () => {
               <p>masuk797@gmail.com</p>
             </div>
           </div>
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3 text-white">
             <FaFacebookF />
             <FaTwitter />
             <FaLinkedinIn />
@@ -27,8 +28,62 @@ const Navbar = () => {
           </div>
         </div>
       </Container>
+
+      <div className="bg-white">
+        <Container>
+          <div className="flex items-center justify-between bg-white py-6">
+            <img src="https://propertydevelopmentltd.com/media/common/PDL-black-logo.png" alt="" className='w-36' />
+            <div className="flex-items-center text-black font-medium">
+              {
+                menuItems && menuItems.map(menu => <Link key={menu?.name} href={menu?.link} className='ml-3'>{menu?.name}</Link>)
+              }
+
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   )
 }
 
 export default Navbar
+
+
+const menuItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Service",
+    link: "/services",
+  },
+  {
+    name: "Solutions",
+    link: "/solutions",
+  },
+  {
+    name: "Projects",
+    link: "/projects",
+  },
+  {
+    name: "Real Estate",
+    link: "/real-estate",
+  },
+  {
+    name: "Notice Board",
+    link: "/notice-board",
+  },
+  {
+    name: "Contact Us",
+    link: "/contact-us",
+  },
+  {
+    name: "real-estate",
+    link: "/real-estate",
+  },
+]
--- src/app/components/ui/WebsitePage/Home/HomeMain.tsx
+++ src/app/components/ui/WebsitePage/Home/HomeMain.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HeroSection from './HeroSection'
 
 const HomeMain = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HeroSection />
+    </div>
   )
 }
 
