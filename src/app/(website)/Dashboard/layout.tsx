bc06b086ea5979521d22cc12391211e8b854f2c1
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-09-08 17:45:49 +0600

    update

--- src/app/(website)/Dashboard/layout.tsx
+++ src/app/(website)/Dashboard/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex gap-5 ">
       <nav className=" ">
-        <Sidebar /> {/* Sidebar should be rendered here */}
+        {/* <Sidebar /> Sidebar should be rendered here */}
       </nav>
       <main className="flex-1 overflow-y-scroll">{children}</main> {/* Main content will be injected here */}
     </div>
