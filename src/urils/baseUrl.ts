5aaf80c04b06a2d9a9700ba4989769bd3f97669e
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-09-08 16:02:43 +0600

    update

--- src/urils/baseUrl.ts
+++ src/urils/baseUrl.ts
@@ -1,8 +1,10 @@
 export const getBaseUrl = () => {
-    return "http://localhost:5000/api/v1"
+    // return "http://localhost:5000/api/v1"
+    return "https://rapid-study-server.onrender.com/api/v1"
 }
 
 
 export const getImageUrl = () => {
-    return "http://168.231.74.216:8010"
+    // return "http://168.231.74.216:8010"
+    return "https://rapid-study-server.onrender.com"
 }
