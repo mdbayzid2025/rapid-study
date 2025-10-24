00a4a1466818acf1b0f1ef6fd9fed6ccab43dc37
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-10-25 01:01:17 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,8 @@ const nextConfig: NextConfig = {
           hostname: "**",
         },
     ]
-  }
+  },
+
 };
 
 export default nextConfig;
--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,15 +1,15 @@
 export const getBaseUrl = () => {
-    return "http://localhost:5000/api/v1"
+    return "http://192.168.0.108:5001/api/v1"
     // return "https://classmanage.crockeriesview.xyz/api/v1"
 }
 
 
 export const getImageUrl = () => {
-    return "http://localhost:5000"
+    return "http://192.168.0.108:5001"
     // return "http://168.231.74.216:8010"
     // return "https://classmanage.crockeriesview.xyz"
 }
 
 
-export const socketUrl = "https://classmanage.crockeriesview.xyz";
-// export const socketUrl = "http://localhost:5000";
+// export const socketUrl = "https://classmanage.crockeriesview.xyz";
+export const socketUrl = "http://192.168.0.108:5001";
