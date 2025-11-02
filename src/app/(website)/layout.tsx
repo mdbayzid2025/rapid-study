e88aa1139340e55273e9a4bccb582f95ea1fc65c
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-11-02 10:46:40 +0600

    update

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -8,7 +8,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
         <Navbar  />
-        <div>
+        <div className='mt-20'>
             {children}
         </div>
         <LandingFooter />
