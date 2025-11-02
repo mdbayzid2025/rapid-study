a2fe04abfac8209bf323ebfa28cfc5b26e21969b
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-11-02 12:42:42 +0600

    update

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,14 +1,14 @@
 export const getBaseUrl = () => {
-    // return "http://161.248.189.80:30012/api/v1"
     return "https://classmanage.crockeriesview.xyz/api/v1"
+    // return "http://class.crockeriesview.xyz/api/v1"
 }
 
 
 export const getImageUrl = () => {
-    // return "http://161.248.189.80:30012"    
-    return "https://classmanage.crockeriesview.xyz"
+    return "https://classmanage.crockeriesview.xyz"    
+    // return "http://class.crockeriesview.xyz"
 }
 
 
-// export const socketUrl = "http://161.248.189.80:30012";
 export const socketUrl = "https://classmanage.crockeriesview.xyz";
+// export const socketUrl = "http://class.crockeriesview.xyz";
